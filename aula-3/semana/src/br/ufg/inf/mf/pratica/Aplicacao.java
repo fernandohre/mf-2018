3c3bb1ea1d8474a394f215a004db708daea48491
Fernando Callata <fernandohre@hotmail.com>
2018-09-10 22:26:00 +0000

    Adicionando nova estrutura de pasta e Aplicacao.java criado através do terminal de comando

---
+++ aula-3/semana/src/br/ufg/inf/mf/pratica/Aplicacao.java
@@ -0,0 +1,8 @@
+package br.ufg.inf.mf.pratica
+import java.time.*;
+
+public class Aplicacao {
+	public static void main(String[] args) {
+		System.out.println(LocalDate.now().getDayOfWeek());
+	}
+}
