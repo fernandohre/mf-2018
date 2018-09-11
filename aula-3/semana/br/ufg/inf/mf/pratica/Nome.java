fec79b4bae0cc611950cc507131031ec52ca48b5
Fernando Callata <fernandohre@hotmail.com>
2018-09-11 00:08:22 +0000

    Exibindo nome da semana de acordo com a configuração local da máquina

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Nome.java
@@ -0,0 +1,16 @@
+package br.ufg.inf.mf.pratica;
+import java.time.*;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Nome {
+	public static void main(String args[]){
+		Locale localAtual = Locale.getDefault();
+		Calendar calendarioAtual = Calendar.getInstance(localAtual);
+		System.out.println(calendarioAtual
+		.getDisplayName(Calendar.DAY_OF_WEEK,
+		 		Calendar.LONG_STANDALONE, 
+				localAtual);
+	}
+
+}
