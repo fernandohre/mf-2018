746d45e1b4a5b1ca84b0199cc773fce599db0309
root <root@PC-606963.localdomain>
2018-09-25 00:25:25 +0000

    programa funcionando em modo depuração, mas esta lançando exceção no split

--- aula-3/csv/src/main/java/br/ufg/inf/aplicacao/Aplicacao.java
+++ aula-3/csv/src/main/java/br/ufg/inf/aplicacao/Aplicacao.java
@@ -10,34 +10,59 @@ import java.io.Reader;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Aplicacao {
     
-    private static String _url = "http://repositorio.dados.gov.br/educacao/CADASTRO%20DAS%20IES_2011.csv";
+    private static String _url = "http://repositorio.dados.gov.br/"
+            + "educacao/CADASTRO%20DAS%20IES_2011.csv";
     private static URL _arquivo;
     private static BufferedReader _leitor;
     private static String _linha = "";
     private static String _separador = ";";
     private static int NUM_LINHAS_IGNORADAS = 10;
     private static int POSICAO_ESTADO = 9;
+    private static Scanner _leia = new Scanner(System.in);
+    private static final HashMap dicionarioDeEstados = new HashMap<String, 
+                                                                   Integer>();
+    private static final Integer UNIVERSIDADE_ENCONTRADA = 1;
     public static void main(String[] args) {
-        int contador = 0;
         try {
-            _arquivo = new URL(_url);
+            String _urlFornecida = _leia.nextLine();
+            if (_urlFornecida.isEmpty()) {
+                _urlFornecida = _url;
+            }
+            _arquivo = new URL(_urlFornecida);
             URLConnection conexao = _arquivo.openConnection();
-            InputStreamReader entrada = new InputStreamReader(conexao.getInputStream());
+            InputStreamReader entrada = new InputStreamReader(conexao
+                                                            .getInputStream());
             _leitor = new BufferedReader(entrada);
+            int contador = 0;
+            int numeroDeUniversidades = 0;
             while ((_linha = _leitor.readLine()) != null) {
                 if (contador < NUM_LINHAS_IGNORADAS) {
+                    contador++;
                     continue;
                 }
                 String partes[] = _linha.split(_separador);
-                System.out.println(partes[POSICAO_ESTADO]);
+                
+                if (!dicionarioDeEstados.containsKey(partes[POSICAO_ESTADO])) {
+                    dicionarioDeEstados.put(partes[POSICAO_ESTADO], 
+                                            UNIVERSIDADE_ENCONTRADA);
+                }
+                int quantidadeDeUniversidades = (int) dicionarioDeEstados
+                                                .get(partes[POSICAO_ESTADO]);
+                dicionarioDeEstados.put(partes[POSICAO_ESTADO], 
+                                        quantidadeDeUniversidades + 1);
             }
-             
+            
+            
         } catch (Exception ex) {
-        
+            System.out.println("Ocorreu um erro! o program será finalizado...");
+            System.out.println(ex.getLocalizedMessage());
         }
-        
     }
 }
