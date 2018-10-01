d03946d61d5d8798f6bf53ca2a8e9147ee03795a
Fernando Callata <fernandohre@hotmail.com>
2018-10-01 23:28:31 +0000

    removendo condição que verifica argumentos digitados pelo usuário, deixando url fixa

--- aula-5/cnes/Formatador/src/main/java/br/ufg/inf/formatador/Formatador.java
+++ aula-5/cnes/Formatador/src/main/java/br/ufg/inf/formatador/Formatador.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URLConnection;
 import java.net.URL;
+import java.util.zip.ZipInputStream;
 
 /**
  * 
@@ -25,18 +26,17 @@ public class Formatador {
     private static URLConnection _conexao;
     private static BufferedReader _leitorDeBuffer;
     private static InputStreamReader _arquivoObtido;
+    private static ZipInputStream _arquivoZipObtido;
     private static String _linha = "";
     private static String SEPARADOR = ";";
     public static void main(String args[]) {
         try {
-            if (args[ENTRADA_DO_USUARIO].isEmpty()) {
-                _arquivo = new URL(_urlFtp);
-            } else {
-                _arquivo = new URL(args[ENTRADA_DO_USUARIO]);
-            }
+            
+            _arquivo = new URL(_urlFtp);
+            
             _conexao = _arquivo.openConnection();
-            _arquivoObtido = new InputStreamReader(_conexao
-                                                           .getInputStream());
+            _arquivoZipObtido = (ZipInputStream) _conexao.getInputStream();
+            _arquivoObtido = new InputStreamReader(_arquivoZipObtido);
             _leitorDeBuffer = new BufferedReader(_arquivoObtido);
             while ((_linha = _leitorDeBuffer.readLine()) != null) {
                 
