6a50da981d0568e65f6f8a67f12ada555bae17ae
Fernando Callata <fernandohre@hotmail.com>
2018-10-09 03:19:50 -0300

    Realizando refatoração marota com a finalidade de aumentar a legibilidade do código

--- aula-3/csv/src/main/java/br/ufg/inf/aplicacao/Aplicacao.java
+++ aula-3/csv/src/main/java/br/ufg/inf/aplicacao/Aplicacao.java
@@ -4,6 +4,7 @@ package br.ufg.inf.aplicacao;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -23,25 +24,16 @@ public class Aplicacao {
     private static BufferedReader leitor;
     private static String linha = "";
     private static String SEPARADOR = ";";
-    private static int NUM_LINHAS_IGNORADAS = 10;
+    private static int NUM_LINHAS_IGNORADAS = 11;
     private static int POSICAO_ESTADO = 9;
     private static Scanner leia = new Scanner(System.in);
-    private static final HashMap dicionarioDeEstados = new HashMap<String, 
-                                                                   Integer>();
+    private static final GerenciadorDicionarioEstados dicionarioDeEstados = 
+            new GerenciadorDicionarioEstados();
     private static final Integer UNIVERSIDADE_ENCONTRADA = 1;
     public static void main(String[] args) {
         try {
-            String urlFornecida = leia.nextLine();
-            if (urlFornecida.isEmpty()) {
-                urlFornecida = url;
-            }
-            arquivo = new URL(urlFornecida);
-            URLConnection conexao = arquivo.openConnection();
-            InputStreamReader entrada = new InputStreamReader(conexao
-                                                            .getInputStream());
-            leitor = new BufferedReader(entrada);
+            leitor = new BufferedReader(obtenhaFluxoDeEntrada());
             int contador = 0;
-            int numeroDeUniversidades = 0;
             while ((linha = leitor.readLine()) != null) {
                 if (contador < NUM_LINHAS_IGNORADAS) {
                     contador++;
@@ -49,25 +41,27 @@ public class Aplicacao {
                 }
                 String partes[] = linha.split(SEPARADOR);
                 if (!(partes.length < POSICAO_ESTADO)) {
-                    if (!dicionarioDeEstados
-                            .containsKey(partes[POSICAO_ESTADO])) {
-                        dicionarioDeEstados.put(partes[POSICAO_ESTADO], 
-                                            UNIVERSIDADE_ENCONTRADA);
-                    }
-                    int quantidadeDeUniversidades = (int) dicionarioDeEstados
-                                                .get(partes[POSICAO_ESTADO]);
-                    dicionarioDeEstados.put(partes[POSICAO_ESTADO], 
-                                        quantidadeDeUniversidades + 1);
-                }
-                dicionarioDeEstados.forEach((chave, valor) -> { 
-                    System.out.println(chave + " " + valor);
-                });
+                    dicionarioDeEstados.adicione(partes[POSICAO_ESTADO]);
+                }   
             }
-            
-            
+            dicionarioDeEstados.exibaQuantidadeDeUniversidadesPorEstado();
         } catch (Exception ex) {
             System.out.println("Ocorreu um erro! o program será finalizado...");
             System.out.println(ex.getLocalizedMessage());
         }
+    }
+    public static InputStreamReader obtenhaFluxoDeEntrada() throws IOException {
+        arquivo = new URL(obtenhaUrl());
+            URLConnection conexao = arquivo.openConnection();
+            InputStreamReader fluxoDeEntrada = new InputStreamReader(conexao
+                                                            .getInputStream());
+        return fluxoDeEntrada;
+    }
+    public static String obtenhaUrl() {
+        String urlFornecida = leia.nextLine();
+        if (urlFornecida.isEmpty()) {
+            urlFornecida = url;
+        }
+        return urlFornecida;
     }
 }
