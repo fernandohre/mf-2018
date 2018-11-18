08cc10d7a9d7f445298463a8cb05df0b49bcb1f2
Fernando Callata <fernandohre@hotmail.com>
2018-11-18 21:45:11 -0200

    mudando tipo de individuo no objeto endereco

--- aula-7/dto/dto/src/main/java/br/ufg/inf/dto/DTOEndereco.java
+++ aula-7/dto/dto/src/main/java/br/ufg/inf/dto/DTOEndereco.java
@@ -5,13 +5,10 @@
  */
 package br.ufg.inf.dto;
 import java.util.Date;
-/**
- *
- * @author ferna_000
- */
+
 public class DTOEndereco {
     private String id;
-    private String individuo;
+    private DTOIndividuo individuo;
     private String bairro;
     private String distrito;
     private long municipio;
@@ -20,6 +17,118 @@ public class DTOEndereco {
     private String caixaPostal;
     private long pais;
     private long tipo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public DTOIndividuo getIndividuo() {
+        return individuo;
+    }
+
+    public void setIndividuo(DTOIndividuo individuo) {
+        this.individuo = individuo;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public long getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(long municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCaixaPostal() {
+        return caixaPostal;
+    }
+
+    public void setCaixaPostal(String caixaPostal) {
+        this.caixaPostal = caixaPostal;
+    }
+
+    public long getPais() {
+        return pais;
+    }
+
+    public void setPais(long pais) {
+        this.pais = pais;
+    }
+
+    public long getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(long tipo) {
+        this.tipo = tipo;
+    }
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(Date dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public Date getDataInicialAcuracia() {
+        return dataInicialAcuracia;
+    }
+
+    public void setDataInicialAcuracia(Date dataInicialAcuracia) {
+        this.dataInicialAcuracia = dataInicialAcuracia;
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public Date getDataFinalAcuracia() {
+        return dataFinalAcuracia;
+    }
+
+    public void setDataFinalAcuracia(Date dataFinalAcuracia) {
+        this.dataFinalAcuracia = dataFinalAcuracia;
+    }
     private Date dataInicial;
     private Date dataInicialAcuracia;
     private Date dataFinal;
---
+++ aula-7/dto/dto/src/main/java/br/ufg/inf/dto/DTOMunicipio.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.dto;
+
+
+public class DTOMunicipio {
+    private long codigo;
+    private String estado;
+    private String municipio;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+}
