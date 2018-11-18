922d972ce919e5b1066902af8743862b5a5709eb
Fernando Callata <fernandohre@hotmail.com>
2018-11-18 15:02:37 -0200

    adicionando dto para ctps e para titulo eleitoral

---
+++ aula-7/dto/dto/src/main/java/br/ufg/inf/dto/DTOCateiraDeTrabalho.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.dto;
+
+/**
+ *
+ * @author ferna_000
+ */
+public class DTOCateiraDeTrabalho {
+    private String identificador;
+    private int serie;
+    private String estado;
+    
+    public String obtenhaIdentificador() {
+        return this.identificador;
+    }
+    public String obtenhaEstado() {
+        return this.estado;
+    }
+    public int obtenhaSerie() {
+        return this.serie;
+    }
+    
+}
---
+++ aula-7/dto/dto/src/main/java/br/ufg/inf/dto/DTOTituloEleitoral.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.dto;
+
+/**
+ *
+ * @author ferna_000
+ */
+public class DTOTituloEleitoral {
+    private String identificador;
+    private int zona;
+    private int sessao;
+    
+    public int obtenhaZona() {
+        return this.zona;
+    }
+    
+    public int obtenhaSessao() {
+        return this.sessao;
+    }
+    
+    public String obtenhaIdentificador() {
+        return this.identificador;
+    }
+}
