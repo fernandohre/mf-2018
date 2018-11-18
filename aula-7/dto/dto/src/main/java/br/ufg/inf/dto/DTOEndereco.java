ce14140c748f3d2297c929b6693bd5523d1050f8
Fernando Callata <fernandohre@hotmail.com>
2018-11-18 16:10:08 -0200

    dto de endereco

---
+++ aula-7/dto/dto/src/main/java/br/ufg/inf/dto/DTOEndereco.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.dto;
+
+/**
+ *
+ * @author ferna_000
+ */
+public class DTOEndereco {
+    private String id;
+    private String individuo;
+    private String bairro;
+    private String distrito;
+    private long municipio;
+    private String estado;
+    private String cep;
+    private String caixaPostal;
+    private long pais;
+    private long tipo;
+    private DateTime dataInicial;
+    private DateTime dataInicialAcuracia;
+    private DateTime dataFinal;
+    private DateTime dataFinalAcuracia;
+}
