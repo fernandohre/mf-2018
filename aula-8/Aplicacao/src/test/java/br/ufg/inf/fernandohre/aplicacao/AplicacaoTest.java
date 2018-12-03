76508cfd00bb28df3121dc3d3dfa89f107c39164
Fernando Callata <fernandohre@hotmail.com>
2018-12-03 20:04:12 -0200

    atualização

---
+++ aula-8/Aplicacao/src/test/java/br/ufg/inf/fernandohre/aplicacao/AplicacaoTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufg.inf.fernandohre.aplicacao;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AplicacaoTest {
+    
+    public AplicacaoTest() {
+    }
+}
